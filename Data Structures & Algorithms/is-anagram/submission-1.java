22d43c601f10ac225cda0503b0f472c8ad34b8af
Sagnik Mukherjee <100195148+C0D3RANG3R@users.noreply.github.com>
2026-05-19 03:44:32 +0530

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length())
+            return false;
+        Map<Character, Integer> map = new HashMap<>();
+        for(char ch:s.toCharArray()) {
+            map.put(ch,map.getOrDefault(ch,0)+1);
+        }
+        for(char ch:t.toCharArray()) {
+            int value = map.getOrDefault(ch,0);
+            if (value == 0)
+                return false;
+            map.put(ch,map.getOrDefault(ch,0)-1);
+        }
+        return true;
+    }
+}
